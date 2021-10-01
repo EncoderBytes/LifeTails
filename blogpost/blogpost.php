669d985dfceb88b9e3bf2e65d9cfc3262e4e561b
Muhammad Osama Javaid <87387879+GitMOJ@users.noreply.github.com>
2021-10-01 13:56:46 +0500

    inital project commit

---
+++ blogpost/blogpost.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/9f56dfd8f1.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Anton&family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;1,400&family=Lexend:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    
+        <link rel="stylesheet" href="../styles/style.css">
+        <link rel="stylesheet" href="../styles/blogpost-style.css">
+
+
+    <title>LifeTails</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-menu flex-row">
+            <div class="nav-brand">
+                <a href="#">LifeTails</a>
+            </div>
+            <ul class="nav-items">
+                <li class="nav-links">
+                    <a href="#">Home</a>
+                </li>
+                <li class="nav-links">
+                    <a href="#">Catagory</a>
+                </li>
+                <li class="nav-links">
+                    <a href="#">Archives</a>
+                </li>
+                <li class="nav-links">
+                    <a href="#">Pages</a>
+                </li>
+                <li class="nav-links">
+                    <a href="#">Contact us</a>
+                </li>
+            </ul>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram-square"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </nav> <hr>
+    
+    <div class="postpublished">
+        <div class="postpublished-post">
+            <div class="postpublished-content">
+                <div class="postpublished-title">
+                    <h1>Now Is The Time For You To Know The Truth About Politics</h1>
+                    <div class="postpublished-info">
+                        <span><i class="fas fa-user text-grey"></i>&nbsp;&nbsp;Admin</span><br>
+                        <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;September 27</span>
+                    </div>
+                    
+                    <div class="postpublished-img">
+                        <img src="../assets/Blog-post/blog1.png" alt="">
+                    </div>               
+                </div>
+
+                <p>
+                    <h4>What is Lorem Ipsum?</h4>
+                    Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been the 
+                    industry's standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting,
+                    remaining essentially unchanged. It was popularised in the 
+                    1960s with the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software like Aldus
+                    PageMaker including versions of Lorem Ipsum.
+                    
+                    <h4>Why do we use it?</h4>
+                    It is a long established fact that a reader will be distracted by
+                    the readable content of a page when looking at its layout. The point 
+                    of using Lorem Ipsum is that it has a more-or-less normal distribution 
+                    of letters, as opposed to using 'Content here, content here', making it 
+                    look like readable English. Many desktop publishing packages and web page 
+                    editors now use Lorem Ipsum as their default model text, and a search for 
+                    'lorem ipsum' will uncover many web sites still in their infancy. Various 
+                    versions have evolved over the years, sometimes by accident, sometimes on 
+                    purpose (injected humour and the like).
+                    
+                    
+                   <h4>Where does it come from?</h4>
+                    Contrary to popular belief, Lorem
+                    Ipsum is not simply random text. It has
+                    roots in a piece of classical Latin literature
+                    from 45 BC, making it over 2000 years old.
+                    Richard McClintock, a Latin professor at Hampden-Sydney
+                    College in Virginia, looked up one of the more obscure 
+                    Latin words, consectetur, from a Lorem Ipsum passage, and going
+                    through the cites of the word in classical literature, discovered 
+                    the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+                    1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) 
+                    by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, 
+                    very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum 
+                    dolor sit amet..", comes from a line in section 1.10.32.
+                    
+                    The standard chunk of Lorem
+                    Ipsum used since the 1500s is reproduced
+                    below for those interested. Sections 1.10.32
+                    and 1.10.33 from "de Finibus Bonorum et Malorum"
+                    by Cicero are also reproduced in their exact original 
+                    form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    
+                    <h4>Where can I get some?</h4>  
+                    There are many variations of passages
+                    of Lorem Ipsum available, but the majority
+                    have suffered alteration in some form, by injected humour, 
+                    or randomised words which don't look even slightly believable. 
+                    If you are going to use a passage of Lorem Ipsum, you need to be sure 
+                    there isn't anything embarrassing hidden in the middle of text. All the 
+                    Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, 
+                    making this the first true generator on the Internet. It uses a dictionary of over 200 
+                    Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum 
+                    which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, 
+                    injected humour, or non-characteristic words etc.
+                </p>
+    
+            </div>
+        </div>
+        <aside>
+            
+            <div class="recentposts">
+                <h1>Recent Posts</h1>
+                    <div class="recentpost-title">
+                        <div class="recentpost-img">
+                            <img src="../assets/popular-post/m-blog-1.jpg" alt="">
+                            <div class="recentpost-info">
+                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;September 27, 2021&nbsp;&nbsp;</span>
+                                <span>2 Comments</span>
+                            </div>
+                        </div>
+                        <a href="#">Now Is The Time For You To Know The Truth About Politics.</a>
+                    </div>
+                    <div class="recentpost-title">
+                        <div class="recentpost-img">
+                            <img src="../assets/popular-post/m-blog-1.jpg" alt="">
+                            <div class="recentpost-info">
+                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;September 27, 2021&nbsp;&nbsp;</span>
+                                <span>2 Comments</span>
+                            </div>
+                        </div>
+                        <a href="#">Now Is The Time For You To Know The Truth About Politics.</a>
+                    </div>
+                    <div class="recentpost-title">
+                        <div class="recentpost-img">
+                            <img src="../assets/popular-post/m-blog-1.jpg" alt="">
+                            <div class="recentpost-info">
+                                <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;September 27, 2021&nbsp;&nbsp;</span>
+                                <span>2 Comments</span>
+                            </div>
+                        </div>
+                        <a href="#">Now Is The Time For You To Know The Truth About Politics.</a>
+                    </div>
+            </div>
+
+            <div class="mostview">
+                <h1>Most view tags</h1>
+                <div class="mostview-tags">
+                    <span class="mostview-tag">Software</span>
+                    <span class="mostview-tag">Study</span>
+                    <span class="mostview-tag">Sports</span>
+                    <span class="mostview-tag">Health</span>
+                    <span class="mostview-tag">Travel</span>
+                    <span class="mostview-tag">Travel</span>
+                    <span class="mostview-tag">Projects</span>
+                    <span class="mostview-tag">Love</span>
+                    <span class="mostview-tag">Design</span>
+                    <span class="mostview-tag">Illustrations</span>
+                    <span class="mostview-tag">Politics</span>
+                </div>
+            </div>
+
+        </aside>
+    </div>
+
+    <footer>
+        <div class="footer-wrapper">
+            <div class="Aboutus">
+                <h2>About us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Nam incidunt eos delectus illum vero ex eligendi, hic vitae, 
+                    corporis quisquam porro voluptatem nesciunt laudantium. 
+                    Libero tempora ipsa dolorum consectetur accusamus.
+                </p>
+            </div>
+
+            <div class="Newsletter">
+                <h2>Newsletter</h2>
+                <p>Stay updated with our latest</p>
+                <input type="Email" placeholder="Email">
+            </div>
+
+            <div class="Followus">
+                <h2>Follow us</h2>
+                <p>Let us be Socail</p>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+            
+            <div class="Footerline">
+                <h4>Copyrights &copy 2021, All rights reserved | made by <a href="#">Muhammad Osama Javaid</a></h4>
+            </div>
+        </div>
+    </footer>    
+
+</body>
+</html>
